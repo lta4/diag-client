95230095fc79d6d07eaed05a11c05f94f0eefce1
Andrew <andrew.mk4@yahoo.com>
2024-07-12 07:47:31 -0700

    roots semi set

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import './App.css';
+
+function App() {
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>
+          <p>Hello Diagnostic</p>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
