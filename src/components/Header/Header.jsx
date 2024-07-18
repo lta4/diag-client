f85e7d043359baff27b48e666a93a3e9f25e3b05
Andrew <andrew.mk4@yahoo.com>
2024-07-18 15:22:37 -0700

    navlink applied

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import "./Header.css";
 import Logo from "../../assets/logo192.png";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
     return (
         <div className="header">
             <img src={Logo} alt="" className="logo"/>
             <ul className="header-menu">
-                <li>Home</li>
-                <li>About</li>
+                <li>
+                    <NavLink to="/">
+                        Home
+                    </NavLink></li>
+                <li>
+                    <NavLink to="About">
+                        About
+                    </NavLink></li>
                 <li>Why Us</li>
                 <li>Plans</li>
                 <li>Testimonials</li>
