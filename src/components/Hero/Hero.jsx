8df3e81fd93c7c0bb1c6247cd2e37bc24ea42967
Andrew <andrew.mk4@yahoo.com>
2024-07-18 11:14:23 -0700

    foundation

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import "./Hero.css";
+import Header from "../Header/Header";
 
 const Hero = () => {
     return (
-        <div>Hero</div>
+        <div className="hero">
+            <div className="left-h">
+                <Header />
+            </div>
+            <div className="right-h">right side</div>
+        </div>
     );
 };
 
