25bacf8c4d43b7e1e7ec71a4a95815040273aff0
Andrew <andrew.mk4@yahoo.com>
2024-07-26 01:25:26 -0700

    footer update

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,11 +3,22 @@ import "./Footer.css";
 
 const Footer = () => {
     return (
-        <footer className="Footer">
+        <div className="footer-container">
+            <hr />
+            <div className="footer">
+                <div className="social-links">
+                    <img />
+                    <img />
+                    <img />
+                </div>
+                <div className="logo-f">
+                    <img />
+                </div>
+            </div>
             © {new Date().getFullYear()}, Built 
              with love by {` `}
             <a href="mailto=andrew4ld@gmail.com" className="footLink">Andrew Arias</a>
-        </footer>
+        </div>
     );
 };
 
