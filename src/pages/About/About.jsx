2a2657982d03a6a3872a8905ce8d3d1a5d7ccaab
Andrew <andrew.mk4@yahoo.com>
2024-07-25 23:46:39 -0700

    update

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -3,7 +3,7 @@ import "./About.css";
 
 const About = () => {
     return (
-        <div className="about">About</div>
+        <div className="about-container">About</div>
     );
 };
 export default About;
