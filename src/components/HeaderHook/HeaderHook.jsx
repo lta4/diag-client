0abc3defad4900002616a4487d468c299ae867cc
Andrew <andrew.mk4@yahoo.com>
2024-08-14 04:27:33 -0700

    headerhook added

---
+++ src/components/HeaderHook/HeaderHook.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import { IoClose, IoMenu } from "react-icons/io5";
+import { useMediaQuery } from "react-responsive";
+import "./HeaderHook.css";
+
+const HeaderHook = () => {
+    return (
+        <div>hook</div>
+    );
+};
+
+export default HeaderHook;
