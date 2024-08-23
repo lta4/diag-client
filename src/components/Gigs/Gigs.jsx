cfa7015307f111785d261ae0a96e655bf2971b7a
Andrew <andrew.mk4@yahoo.com>
2024-08-23 11:26:52 -0700

    fushion container added

--- src/components/Gigs/Gigs.jsx
+++ src/components/Gigs/Gigs.jsx
@@ -10,6 +10,35 @@ const Gigs = () => {
             <div className="right-g">
                 <p>right ggg</p>
             </div>
+            <div className="fushion-container" id="shows">
+                <div className="fushion-title">
+                    <div className="bit-container">
+                        <div className="bit-navbar-container">
+                            <div className="bit-events-lists">
+                                <div className="bit-events-container">
+                                    <div className="bit-upcoming-events">
+                                        <div className="bit-event">
+                                            <a className="bit-details">
+                                                <div>
+                                                    <span className="bit-date"></span>
+                                                </div>
+                                                <div className="bit-titleWrapper">
+                                                    <div className="bit-venue"></div>
+                                                </div>
+                                                <div className="bit-location"></div>
+                                            </a>
+                                            <div className="bit-details bit-event-buttons">
+                                                <div></div>
+                                                <div></div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
