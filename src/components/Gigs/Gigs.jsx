81b7abd102bc64e7e77cfd9729c1fa20ede027cd
Andrew <andrew.mk4@yahoo.com>
2024-08-27 14:13:51 -0700

    update

--- src/components/Gigs/Gigs.jsx
+++ src/components/Gigs/Gigs.jsx
@@ -11,26 +11,43 @@ const Gigs = () => {
                 <p>right ggg</p>
             </div> */}
             <div className="fushion-container" id="shows">
-                <div className="fushion-title">Gig Dates
-                    <div className="bit-container">
-                        <div className="bit-navbar-container">
-                            <div className="bit-events-lists">
-                                <div className="bit-events-container">
-                                    <div className="bit-upcoming-events">
-                                        <div className="bit-event">
-                                            <a className="bit-details">
-                                                <div>
-                                                    <span className="bit-date">Fri Aug 23</span>
-                                                </div>
-                                                <div className="bit-titleWrapper">
-                                                    <div className="bit-venue">Music Box</div>
-                                                </div>
-                                                <div className="bit-location">San Diego, CA</div>
-                                            </a>
-                                            <div className="bit-details bit-event-buttons">
-                                                <div className="bit-rsvp-container"></div>
-                                                <a className="bit-rsvp bit-button" href="">RSVP</a>
+                <div className="fushion-title">
+                    <h3 className="fushion-gig">Gig Dates</h3>
+                </div>
+            </div>
+                <div className="bit-container">
+                    <div className="bit-navbar-container">
+                    <div className="bit-event-lists">
+                        <div className="bit-events-container">
+                            <div className="bit-upcoming-events">
+                                <div className="bit-event">
+                                    <a className="bit-details">
+                                        <div>
+                                            <span className="bit-date">Fri Aug 23</span>
+                                        </div>
+                                        <div className="bit-titleWrapper">
+                                            <div className="bit-venue">Music Box</div>
                                             </div>
+                                        <div className="bit-location">San Diego, CA</div>
+                                    </a>
+                                    <div className="bit-details bit-event-buttons">
+                                        <div className="bit-rsvp-container"></div>
+                                        <a className="bit-rsvp bit-button" href="">RSVP</a>
+                                    </div>
+                                </div>
+                                <div className="bit-event">
+                                    <a className="bit-details">
+                                        <div>
+                                            <span className="bit-date">Sun Oct 1</span>
+                                        </div>
+                                        <div className="bit-titleWrapper">
+                                            <div className="bit-venue">Radys Stadium</div>
+                                            </div>
+                                        <div className="bit-location">San Diego, CA</div>
+                                    </a>
+                                    <div className="bit-details bit-event-buttons">
+                                        <div className="bit-rsvp-container">
+                                        <a className="bit-rsvp bit-button" href="">RSVP</a>
                                         </div>
                                     </div>
                                 </div>
