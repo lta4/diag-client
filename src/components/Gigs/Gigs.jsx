7b30015b44ded7e6f99c8a47c6ff432524ffbddf
Andrew <andrew.mk4@yahoo.com>
2024-08-28 16:11:38 -0700

    gigs with flex

--- src/components/Gigs/Gigs.jsx
+++ src/components/Gigs/Gigs.jsx
@@ -20,15 +20,11 @@ const Gigs = () => {
                             <div className="bit-event-lists">
                                 <div className="bit-events-container">
                                     <div className="bit-upcoming-events">
-                                        <div className="bit-event">
-                                            <div className="bit-details">
-                                                <div>
-                                                    <span className="bit-date">Fri Aug 23</span>
-                                                </div>
-                                                <div className="bit-titleWrapper">
-                                                    <div className="bit-venue">Music Box</div>
-                                                    </div>
-                                                <div className="bit-location">San Diego, CA</div>
+                                        <div className="g-event">
+                                            <div className="g-details">
+                                                <div className="g-date">Fri Aug 23</div>
+                                                <div className="g-venue">Music Box</div>
+                                                <div className="g-location">San Diego, CA</div>
                                             </div>
                                             <div className="bit-details bit-event-buttons">
                                                 <div className="bit-rsvp-container">
@@ -36,15 +32,11 @@ const Gigs = () => {
                                                 </div>
                                             </div>
                                         </div>
-                                        <div className="bit-event">
-                                            <div className="bit-details">
-                                                <div>
-                                                    <span className="bit-date">Sun Oct 1</span>
-                                                </div>
-                                                <div className="bit-titleWrapper">
-                                                    <div className="bit-venue">Radys Stadium</div>
-                                                    </div>
-                                                <div className="bit-location">San Diego, CA</div>
+                                        <div className="g-event">
+                                            <div className="g-details">
+                                                <div className="g-date">Sun Oct 1</div>
+                                                <div className="g-venue">Radys Stadium</div>
+                                                <div className="g-location">San Diego, CA</div>
                                             </div>
                                             <div className="bit-details bit-event-buttons">
                                                 <div className="bit-rsvp-container">
@@ -52,15 +44,11 @@ const Gigs = () => {
                                                 </div>
                                             </div>
                                         </div>
-                                        <div className="bit-event">
-                                            <div className="bit-details">
-                                                <div>
-                                                    <span className="bit-date">Mon Dec 4</span>
-                                                </div>
-                                                <div className="bit-titleWrapper">
-                                                    <div className="bit-venue">Sycamore Den</div>
-                                                    </div>
-                                                <div className="bit-location">San Diego, CA</div>
+                                        <div className="g-event">
+                                            <div className="g-details">
+                                                <div className="g-date">Mon Dec 4</div>
+                                                <div className="g-venue">Sycamore Den</div>
+                                                <div className="g-location">San Diego, CA</div>
                                             </div>
                                             <div className="bit-details bit-event-buttons">
                                                 <div className="bit-rsvp-container">
@@ -68,6 +56,26 @@ const Gigs = () => {
                                                 </div>
                                             </div>
                                         </div>
+    <div class="tour">
+    <div class="row heading">
+      <div class="cell">Category 1</div>
+      <div class="cell">Category 2</div>
+      <div class="cell">Category 3</div>
+      <div class="cell">Category 4</div>
+    </div>
+    <div class="row">
+      <div class="cell">Item 1</div>
+      <div class="cell">Item 2</div>
+      <div class="cell">Item 3</div>
+      <div class="cell">Item 4</div>
+    </div>
+    <div class="row">
+     <div class="cell">Item 1</div>
+      <div class="cell">Item 2</div>
+      <div class="cell">Item 3</div>
+      <div class="cell">Item 4</div>
+    </div>
+  </div>
                                     </div>
                                 </div>
                             </div>
