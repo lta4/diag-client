c2757b4c83cd01c7cd46a7ff895d3e6205102d16
Andrew <andrew.mk4@yahoo.com>
2024-09-06 12:26:12 -0700

    update

--- src/components/Gigs/Gigs.jsx
+++ src/components/Gigs/Gigs.jsx
@@ -10,43 +10,39 @@ const Gigs = () => {
                         Gig Dates
                     </h3>
                 </div>
-                                <div className="show-container">
-                                    <div className="show-events">
-                                        <div className="g-event">
-                                            <div className="g-details">
-                                                <div className="g-date">Fri Aug 23</div>
-                                                <div className="g-venue">Music Box</div>
-                                                <div className="g-location">San Diego, CA</div>
-                                                <div className="g-button-container">
-                                                    <a className="g-button" href="">RSVP</a>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div className="g-event">
-                                            <div className="g-details">
-                                                <div className="g-date">Sun Oct 1</div>
-                                                <div className="g-venue">Radys Stadium</div>
-                                                <div className="g-location">San Diego, CA</div>
-                                                <div className="g-button-container">
-                                                    <a className="g-button" href="">RSVP</a>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div className="g-event">
-                                            <div className="g-details">
-                                                <div className="g-date">Mon Dec 4</div>
-                                                <div className="g-venue">Sycamore Den</div>
-                                                <div className="g-location">San Diego, CA</div>
-                                                <div className="g-button-container">
-                                                    <a className="g-button" href="">RSVP</a>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                
-                            
-                        
-                    
+                <div className="show-container">
+                    <div className="show-events">
+                        <div className="g-event">
+                            <div className="g-details">
+                                <div className="g-date">Fri Aug 23</div>
+                                <div className="g-venue">Music Box</div>
+                                <div className="g-location">San Diego, CA</div>
+                                <div className="g-button-container">
+                                <a className="g-button" href="">RSVP</a>
+                            </div>
+                        </div>
+                    </div>
+                        <div className="g-event">
+                            <div className="g-details">
+                                <div className="g-date">Sun Oct 1</div>
+                                <div className="g-venue">Radys Stadium</div>
+                                <div className="g-location">San Diego, CA</div>
+                                <div className="g-button-container">
+                                <a className="g-button" href="">RSVP</a>
+                            </div>
+                        </div>
+                    </div>
+                        <div className="g-event">
+                            <div className="g-details">
+                                <div className="g-date">Mon Dec 4</div>
+                                <div className="g-venue">Sycamore Den</div>
+                                <div className="g-location">San Diego, CA</div>
+                                <div className="g-button-container">
+                                <a className="g-button" href="">RSVP</a>
+                            </div>
+                        </div>
+                    </div>
+                    </div>
                 </div>
             </div>
         </div>
