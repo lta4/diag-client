8fa77cad1c83e761e5fe249ed492fdbde09bcaad
Andrew <andrew.mk4@yahoo.com>
2025-02-17 04:12:52 -0800

    poster added

--- src/pages/Video/Video.jsx
+++ src/pages/Video/Video.jsx
@@ -5,8 +5,7 @@ import "@vidstack/react/player/styles/default/layouts/video.css";
 import { MediaPlayer, MediaProvider } from "@vidstack/react";
 import { defaultLayoutIcons, DefaultVideoLayout } from
 "@vidstack/react/player/layouts/default";
-import { Poster, type PosterProps } from "@vidstack/react";
-import JSix from "../../assets/jSix.jpeg";
+import { Poster } from "@vidstack/react";
 
 const Plans = () => {
     return (
@@ -15,9 +14,12 @@ const Plans = () => {
                 Video
                 <MediaPlayer title="Sprite Fight" src="https://files.vidstack.io/sprite-fight/720p.mp4">
                     <MediaProvider />
-                    <DefaultVideoLayout thumbnails="https://files.vidstack.io/sprite-fight/thumbnails.vtt" icons={defaultLayoutIcons} 
-                    />
-                    <Poster src="{Jsix}" alt="..." />
+                        <Poster 
+                            className="vds-poster"
+                            src="https://files.vidstack.io/sprite-fight/poster.webp" 
+                            alt="Girl walks into campfire with gnomes surrounding her friend ready for their next meal!" />
+                        <DefaultVideoLayout thumbnails="https://files.vidstack.io/sprite-fight/thumbnails.vtt" icons={defaultLayoutIcons} 
+                        />
                 </MediaPlayer>
             </div>
         </>
