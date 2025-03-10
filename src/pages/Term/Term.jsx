b11a951839d1fd74eb2305838ef26a97067fc96f
Andrew <andrew.mk4@yahoo.com>
2025-03-10 04:47:38 -0700

    post added

--- src/pages/Term/Term.jsx
+++ src/pages/Term/Term.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./Term.css";
 import PersonList from "../../components/PersonList/PersonList";
+import PersonAdd from "../../components/PersonAdd/PersonAdd";
 
 const Term = () => {
     return (
         <div className="legal">
+            <PersonAdd />
             <PersonList />
         </div>
     );
