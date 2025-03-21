b2da213f4abd7cc49213c6ae69d885d9a777a39a
Andrew <andrew.mk4@yahoo.com>
2025-03-21 04:42:31 -0700

    attempts getting bands api key

--- src/components/Show/Show.jsx
+++ src/components/Show/Show.jsx
@@ -1,23 +1,35 @@
 import React from "react";
 import axios from "axios";
 
-const Show = "https://jsonplaceholder.typicode.com/posts/1";
+const baseURL = "https://jsonplaceholder.typicode.com/posts";
 
 export default function App() {
-    const [post, setPost] = React.useState(null);
+  const [post, setPost] = React.useState(null);
 
-    React.useEffect(() => {
-        axios.get(Show).then((response) => {
-            setPost(response.data);
-        });
-    }, []);
+  React.useEffect(() => {
+    axios.get(`${baseURL}/1`).then((response) => {
+      setPost(response.data);
+    });
+  }, []);
 
-    if (!post) return null;
+  function createPost() {
+    axios
+      .post(baseURL, {
+        title: "Hello World!",
+        body: "This is a new post."
+      })
+      .then((response) => {
+        setPost(response.data);
+      });
+  }
 
-    return (
-        <div>
-            <h1>{post.title}</h1>
-            <p>{post.body}</p>
-        </div>
-    );
-};
+  if (!post) return "No post!"
+
+  return (
+    <div>
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
+      <button onClick={createPost}>Create Post</button>
+    </div>
+  );
+}
