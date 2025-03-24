fa90dee24b39f16cbb10b65743f18fa3a50ee100
Andrew <andrew.mk4@yahoo.com>
2025-03-24 03:57:00 -0700

    mix adjustment

--- src/components/Mix/Mix.jsx
+++ src/components/Mix/Mix.jsx
@@ -16,41 +16,41 @@ const Mix = () => {
                     <div className="songs__list">
                         <div className="songs__list--name">
                             <a href="">
-                            <iframe width="560" height="315" src="https://www.youtube.com/embed/t4neSm8RiME?si=-wLwyG8Wzfw71I_F" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
+                            <iframe width="400" height="315" src="https://www.youtube.com/embed/t4neSm8RiME?si=-wLwyG8Wzfw71I_F" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
                             </iframe>
-                            EP.002
+                            {/* EP.002 */}
                             </a>
                         </div>
                         <div className="songs__list--name">
                             <a href="">
-                            <iframe width="560" height="315" src="https://www.youtube.com/embed/3tLRhUGOqM4?si=d9mXK__A1d68uKyy" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
+                            <iframe width="400" height="315" src="https://www.youtube.com/embed/3tLRhUGOqM4?si=d9mXK__A1d68uKyy" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
                             </iframe>
-                                EP.003
+                                {/* EP.003 */}
                             </a>
                         </div>
                         <div className="songs__list--name">
                             <a href="">
-                            <iframe width="560" height="315" src="https://www.youtube.com/embed/AYnJ1ur4Sd8?si=Papf1DTncuUMsKqu" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
+                            <iframe width="400" height="315" src="https://www.youtube.com/embed/AYnJ1ur4Sd8?si=Papf1DTncuUMsKqu" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
                             </iframe>
-                                EP.004
+                                {/* EP.004 */}
                             </a>
                         </div>
                         <div className="songs__list--name">
                             <a href="">
-                            <iframe width="560" height="315" src="https://www.youtube.com/embed/0t_ZjqrGkVw?si=ir46CtaJp-umZsFw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-                            BEFSKII B2B Diagnostic
+                            <iframe width="400" height="315" src="https://www.youtube.com/embed/0t_ZjqrGkVw?si=ir46CtaJp-umZsFw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                            {/* Diagnostic */}
                             </a>
                         </div>
-                        <div className="songs__list--name">
+                        {/* <div className="songs__list--name">
                             <a href="">
                                 sullivan king
                             </a>
-                        </div>
-                        <div className="songs__list--name">
+                        </div> */}
+                        {/* <div className="songs__list--name">
                             <a href="">
                                 kaskade
                             </a>
-                        </div>
+                        </div> */}
                     </div>
                 </div>
             </div>
