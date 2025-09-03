a292831282f3448ccc481cfce216cab369794442
Andrew <andrew.mk4@yahoo.com>
2025-09-02 23:11:49 -0500

    songs back color

--- src/components/Mix/Mix.jsx
+++ src/components/Mix/Mix.jsx
@@ -12,47 +12,45 @@ const Mix = () => {
                 </div>
             </div>
             <div className="songs">
-                <div className="songs__event">
-                    <div className="songs__list">
-                        <div className="songs__list--section">
-                            <a href="" className="songs__list--name">
-                            <iframe width="560" height="315" src="https://www.youtube.com/embed/t4neSm8RiME?si=-wLwyG8Wzfw71I_F" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
-                            </iframe>
-                                EP.002
-                            </a>
-                        </div>
-                        <div className="songs__list--section">
-                            <a href="" className="songs__list--name">
-                            <iframe width="560" height="315" src="https://www.youtube.com/embed/3tLRhUGOqM4?si=d9mXK__A1d68uKyy" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
-                            </iframe>
-                                EP.003
-                            </a>
-                        </div>
-                        <div className="songs__list--section">
-                            <a href="" className="songs__list--name">
-                            <iframe width="560" height="315" src="https://www.youtube.com/embed/AYnJ1ur4Sd8?si=Papf1DTncuUMsKqu" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
-                            </iframe>
-                                EP.004
-                            </a>
-                        </div>
-                        <div className="songs__list--section">
-                            <a href="" className="songs__list--name">
-                            <iframe width="560" height="315" src="https://www.youtube.com/embed/0t_ZjqrGkVw?si=ir46CtaJp-umZsFw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
-                            </iframe>
-                                Diagnostic
-                            </a>
-                        </div>
-                        {/* <div className="songs__list--name">
-                            <a href="">
-                                sullivan king
-                            </a>
-                        </div> */}
-                        {/* <div className="songs__list--name">
-                            <a href="">
-                                kaskade
-                            </a>
-                        </div> */}
+                <div className="songs__list">
+                    <div className="songs__list--section">
+                        <a href="" className="songs__list--name">
+                        <iframe width="560" height="315" src="https://www.youtube.com/embed/t4neSm8RiME?si=-wLwyG8Wzfw71I_F" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
+                        </iframe>
+                            EP.002
+                        </a>
                     </div>
+                    <div className="songs__list--section">
+                        <a href="" className="songs__list--name">
+                        <iframe width="560" height="315" src="https://www.youtube.com/embed/3tLRhUGOqM4?si=d9mXK__A1d68uKyy" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
+                        </iframe>
+                            EP.003
+                        </a>
+                    </div>
+                    <div className="songs__list--section">
+                        <a href="" className="songs__list--name">
+                        <iframe width="560" height="315" src="https://www.youtube.com/embed/AYnJ1ur4Sd8?si=Papf1DTncuUMsKqu" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
+                        </iframe>
+                            EP.004
+                        </a>
+                    </div>
+                    <div className="songs__list--section">
+                        <a href="" className="songs__list--name">
+                        <iframe width="560" height="315" src="https://www.youtube.com/embed/0t_ZjqrGkVw?si=ir46CtaJp-umZsFw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
+                        </iframe>
+                            Diagnostic
+                        </a>
+                    </div>
+                    {/* <div className="songs__list--name">
+                        <a href="">
+                            sullivan king
+                        </a>
+                    </div> */}
+                    {/* <div className="songs__list--name">
+                        <a href="">
+                            kaskade
+                        </a>
+                    </div> */}
                 </div>
             </div>
         </div>
