dc43213baba73e542ed6f8b90b63f11750ed8923
Andrew <andrew.mk4@yahoo.com>
2025-09-04 01:10:02 -0500

    update to fail medium will not create exact endpoint

--- src/components/Gigs/Gigs.jsx
+++ src/components/Gigs/Gigs.jsx
@@ -44,7 +44,7 @@ const Gigs = () => {
                 </div>
             </div>
             <div>
-                <Show />
+                {/* <Show /> */}
             </div>
         </div>
     </div>
