f905b6e9e9c666e844fc6e05029153a08f1cbe11
Andrew <andrew.mk4@yahoo.com>
2025-09-04 00:49:37 -0500

    axios update

--- src/components/Show/Show.jsx
+++ src/components/Show/Show.jsx
@@ -1,35 +1,58 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-const baseURL = "https://jsonplaceholder.typicode.com/posts";
+// const baseURL = "https://andrew4ld.medium.com/mix-22459474be3a?source=friends_link&sk=9299dc4e69981caf0662596f217e3687/posts/1";
 
-export default function App() {
-  const [post, setPost] = React.useState(null);
+// export default function App() {
+//   const [post, setPost] = React.useState(null);
 
-  React.useEffect(() => {
-    axios.get(`${baseURL}/1`).then((response) => {
-      setPost(response.data);
-    });
-  }, []);
+//   React.useEffect(() => {
+//     axios.get(baseURL).then((response) => {
+//       setPost(response.data);
+//     });
+//   }, []);
 
-  function createPost() {
+//   if (!post) return null;
+
+//   return (
+//     <div>
+//       <h1>{post.title}</h1>
+//       <p>{post.body}</p>
+//     </div>
+//   );
+// }
+
+const Show = () => {
+
+  const [posts, setPosts] = useState([]);
+  const getPostData = () => {
     axios
-      .post(baseURL, {
-        title: "Hello World!",
-        body: "This is a new post."
+      .get("https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@andrew4ld")
+      .then((res) => {
+        setPosts(res.data.items);
       })
-      .then((response) => {
-        setPost(response.data);
+      .catch((error) => {
+        console.error("Error fetching blog posts:", error);
       });
-  }
+  };
 
-  if (!post) return "No post!"
+  useEffect(() => {
+    getPostData();
+  }, []);
 
   return (
     <div>
-      <h1>{post.title}</h1>
-      <p>{post.body}</p>
-      <button onClick={createPost}>Create Post</button>
+      {posts.map((post) => {
+        <div key={post.guid}>
+          <h2>{post.title}</h2>
+          <p dangerouslySetInnerHTML={{ __html: post.content }} />
+          <a href={post.link} target="_blank" rel="noopener noreferrer">
+            Read More
+          </a>
+        </div>
+      })}
     </div>
   );
-}
+};
+
+export default Show;
