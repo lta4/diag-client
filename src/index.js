dea6e6a49a03994cf9d5ea6cc757d2fc0c14991c
Andrew <andrew.mk4@yahoo.com>
2025-09-29 01:35:06 -0500

    high clean up in index

--- src/index.js
+++ src/index.js
@@ -3,18 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <Router> */}
       <App />
       {/* <Routes> */}
         {/* <Route path="/"> */}
         {/* </Route> */}
       {/* </Routes> */}
-    {/* </Router> */}
   </React.StrictMode>
   // document.body
 );
