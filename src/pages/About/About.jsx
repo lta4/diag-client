611d5a16c19fa35d1478c7da0d0921cbd391633e
Andrew <andrew.mk4@yahoo.com>
2025-09-28 23:04:33 -0500

    about refreshed

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -3,6 +3,7 @@ import "./About.css";
 import JTwo from "../../assets/jTwo.jpg"
 import JSix from "../../assets/jSix.jpeg";
 import JEleven from "../../assets/jEleven.jpeg";
+import JThirteen from "../../assets/jThirteen.jpg";
 import ScrollToTop from "react-scroll-to-top";
 
 const About = () => {
@@ -11,25 +12,28 @@ const About = () => {
             <section className="profile">
                 <div className="profile__set">
                     <div className="profile__container">
-                    <img src={JEleven} alt="" className="profile__container--image" />
-                    <div className="profile__container--text">
-                        Diagnostic
-                    </div>
-                    {/* <div className="profile__set--description">
+                    <img src={JThirteen} alt="" className="profile__container--image" />
+                    {/* <div className="profile__container--text">
                         <h2>Jose Flores</h2>
                         <p>I'm a DJ residing in San Diego, CA.
                         I have a serious passion for creating amazing styles and intuitive musical experiences.</p>
                         <p>Let's create something special!</p>
                     </div> */}
                     </div>
+                    <div className="profile__description">
+                    <h1 className="profile__description--title">Jose Flores</h1>
+                        <p className="profile__description--text">
+                        Welcome to Diagnostic & Friends, your gateway to the world of house music! Join us on a sonic journey through the realms of underground beats and timeless classics. Our YouTube channel is your destination for electrifying house music mixes accompanied by captivating visuals. Explore the sounds of the past and the cutting-edge tunes of today as we curate the ultimate internet radio show experience. Subscribe now and dive into the groove.
+                        </p>
+                    </div>
                 </div>
                 <div className="profile__set">
-                    <div className="profile__set--description">
+                    {/* <div className="profile__set--description">
                     <h1 className="profile__set--title">Jose Flores</h1>
                         <p>
                         Welcome to Diagnostic & Friends, your gateway to the world of house music! Join us on a sonic journey through the realms of underground beats and timeless classics. Our YouTube channel is your destination for electrifying house music mixes accompanied by captivating visuals. Explore the sounds of the past and the cutting-edge tunes of today as we curate the ultimate internet radio show experience. Subscribe now and dive into the groove.
                         </p>
-                    </div>
+                    </div> */}
                 </div>
             </section>
             <ScrollToTop />
