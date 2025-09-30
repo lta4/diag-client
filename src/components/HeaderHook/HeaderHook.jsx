75e4f8e006f2ac49df8951408d0f584373510907
Andrew <andrew.mk4@yahoo.com>
2025-09-29 22:41:19 -0500

    header headerhook merged

--- src/components/HeaderHook/HeaderHook.jsx
+++ src/components/HeaderHook/HeaderHook.jsx
@@ -1,17 +1,16 @@
 import React, { useState } from "react";
-// import CDiagBanner from "../../assets/CDiagBanner.jpg";
 import WDiag from "../../assets/wDiag.png";
 import { NavLink } from "react-router-dom";
 import { IoClose, IoMenu } from "react-icons/io5";
 import { useMediaQuery } from "react-responsive";
+import "./Header.css";
 import "./HeaderHook.css";
 import { HashLink as Link } from "react-router-hash-link";
 
 const HeaderHook = () => {
-    
     const [isMenuOpen, setIsMenuOpen] = useState(false);
     const isMobile = useMediaQuery({ maxWidth: "1150px" });
-    
+
     const toggleMenu = () => {
         setIsMenuOpen(!isMenuOpen);
     };
@@ -21,77 +20,46 @@ const HeaderHook = () => {
         }
     };
 
-    const renderNavLinks = () => {
-        const listClassName = isMobile ? "nav__list" : "nav__list__web";
-        const linkClassName = "nav__link";
-        const buttonClassName = "nav__cta";
-    
-
-    return (
-        <ul className={listClassName}>
-            <li>
-                <Link smooth to="#shows" className={linkClassName} onClick={closeMobileMenu}>
-                    Shows
-                </Link>
-            </li>
-            <li>
-                <Link smooth to="#mix" className={linkClassName} onClick={closeMobileMenu}>
-                    Mixes
-                </Link>
-            </li>
-            <li>
-                <Link smooth to="#inquire" className={linkClassName} onClick={closeMobileMenu}>
-                    Inquire
-                </Link>
-            </li>
-            <li>
-                <NavLink to="/About" className={linkClassName} onClick={closeMobileMenu}>
-                    About
-                </NavLink>
-            </li>
-            <li>
-                <NavLink to="/Video" className={linkClassName} onClick={closeMobileMenu}>
-                    Video
-                </NavLink>
-            </li>
-            <li>
-                <NavLink to="/" className={`${linkClassName} ${buttonClassName}`} onClick={closeMobileMenu}>
-                    Get Started
-                </NavLink>
-            </li>
-        </ul>
-        );
-    };
+    const listClassName = isMobile ? "nav__list" : "nav__list__web";
+    const linkClassName = "nav__link";
+    const buttonClassName = "nav__cta";
 
     return (
         <header className="header">
             <nav className="nav active">
-                {/* <img src={CDiagBanner} alt="" className="CDiagBanner"/> */}
                 <NavLink to="/" className="nav__logo">
-                    {/* DIAGNOSTIC */}
-                    <a href="" className="nav__wDiag">
-                    <img src={WDiag} alt="" className="nav__wDiag"/>
-                    </a>
+                    <span className="nav__wDiag">
+                        <img src={WDiag} alt="Logo" className="nav__wDiag" />
+                    </span>
                 </NavLink>
-                
                 {isMobile && (
                     <div className="nav__toggle" id="nav-toggle" onClick={toggleMenu}>
                         <IoMenu />
                     </div>
                 )}
-
                 {isMobile ? (
-                    <div 
-                        className={`nav__menu ${isMenuOpen ? "show-menu" : ""}`}
-                        id="nav-menu"
-                    >
-                        {renderNavLinks()}
+                    <div className={`nav__menu ${isMenuOpen ? "show-menu" : ""}`} id="nav-menu">
+                        <ul className={listClassName}>
+                            <li><Link smooth to="#shows" className={linkClassName} onClick={closeMobileMenu}>Shows</Link></li>
+                            <li><Link smooth to="#mix" className={linkClassName} onClick={closeMobileMenu}>Mixes</Link></li>
+                            <li><Link smooth to="#inquire" className={linkClassName} onClick={closeMobileMenu}>Inquire</Link></li>
+                            <li><NavLink to="/About" className={linkClassName} onClick={closeMobileMenu}>About</NavLink></li>
+                            <li><NavLink to="/Video" className={linkClassName} onClick={closeMobileMenu}>Video</NavLink></li>
+                            <li><NavLink to="/" className={`${linkClassName} ${buttonClassName}`} onClick={closeMobileMenu}>Get Started</NavLink></li>
+                        </ul>
                         <div className="nav__close" id="nav-close" onClick={toggleMenu}>
                             <IoClose />
                         </div>
                     </div>
                 ) : (
-                    renderNavLinks()
+                    <ul className={listClassName}>
+                        <li><Link smooth to="#shows" className={linkClassName}>Shows</Link></li>
+                        <li><Link smooth to="#mix" className={linkClassName}>Mixes</Link></li>
+                        <li><Link smooth to="#inquire" className={linkClassName}>Inquire</Link></li>
+                        <li><NavLink to="/About" className={linkClassName}>About</NavLink></li>
+                        <li><NavLink to="/Video" className={linkClassName}>Video</NavLink></li>
+                        <li><NavLink to="/" className={`${linkClassName} ${buttonClassName}`}>Get Started</NavLink></li>
+                    </ul>
                 )}
             </nav>
         </header>
