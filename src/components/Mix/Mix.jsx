1cb5cabdf15e23f77b7ec474452169afbe1b816c
Andrew <andrew.mk4@yahoo.com>
2025-10-19 11:02:19 -0500

    update

--- src/components/Mix/Mix.jsx
+++ src/components/Mix/Mix.jsx
@@ -14,40 +14,37 @@ const Mix = () => {
             <div className="songs">
                 <div className="songs__list">
                     <div className="songs__list--section">
-                        <iframe width="560" height="315"
+                        <iframe 
+                        title="Diagnostic & Friends 002 - Youtube"
+                        width="560" height="315"
                         className="songs__list--name" 
-                        src="https://www.youtube.com/embed/t4neSm8RiME?si=-wLwyG8Wzfw71I_F" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
+                        src="https://www.youtube.com/embed/t4neSm8RiME?si=-wLwyG8Wzfw71I_F" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
                         </iframe>
                     </div>
                     <div className="songs__list--section">
-                        <iframe width="560" height="315"
+                        <iframe
+                        title="Diagnostic & Friends 003 - Youtube" 
+                        width="560" height="315"
                         className="songs__list--name" 
-                        src="https://www.youtube.com/embed/3tLRhUGOqM4?si=d9mXK__A1d68uKyy" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
+                        src="https://www.youtube.com/embed/3tLRhUGOqM4?si=d9mXK__A1d68uKyy" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
                         </iframe>
                     </div>
                     <div className="songs__list--section">
-                        <iframe width="560" height="315"
+                        <iframe
+                        title="Diagnostic & Friends 004 - Youtube"
+                        width="560" height="315"
                         className="songs__list--name" 
-                        src="https://www.youtube.com/embed/AYnJ1ur4Sd8?si=Papf1DTncuUMsKqu" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
+                        src="https://www.youtube.com/embed/AYnJ1ur4Sd8?si=Papf1DTncuUMsKqu" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
                         </iframe>
                     </div>
                     <div className="songs__list--section">
-                        {/* <a href="" className="songs__list--name"> */}
-                        <iframe width="560" height="315"
+                        <iframe
+                        title="Diagnostic @ Parc & Rec - Youtube"
+                        width="560" height="315"
                         className="songs__list--name" 
-                        src="https://www.youtube.com/embed/0t_ZjqrGkVw?si=ir46CtaJp-umZsFw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
+                        src="https://www.youtube.com/embed/0t_ZjqrGkVw?si=ir46CtaJp-umZsFw" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
                         </iframe>
                     </div>
-                    {/* <div className="songs__list--name">
-                        <a href="">
-                            sullivan king
-                        </a>
-                    </div> */}
-                    {/* <div className="songs__list--name">
-                        <a href="">
-                            kaskade
-                        </a>
-                    </div> */}
                 </div>
             </div>
         </div>
