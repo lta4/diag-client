b655f2731293e337aed3cd465342a0cc2577c1f6
Andrew <andrew.mk4@yahoo.com>
2025-10-19 23:12:50 -0500

    about updated

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import "./About.css";
 import JThirteen from "../../assets/jThirteen.jpg";
+import JSixteen from "../../assets/jSixteen.jpg";
 import ScrollToTop from "react-scroll-to-top";
 
 function About() {
@@ -13,21 +14,19 @@ function About() {
         <>
             <section className="profile">
                 <div className="profile__set">
-                    <div className="profile__container">
-                    <img src={JThirteen} alt="" className="profile__container--image" />
-                    </div>
-                    <div className="profile__description">
-                    <h1 className="profile__description--title">Jose Flores</h1>
-                        {/* <p className="profile__description--text">
-                        My name is Jose Flores and welcome to Diagnostic & Friends, your gateway to the world of house music. Join us on a sonic journey through the realms of underground beats and timeless classics. My YouTube channel is your destination for electrifying latin house mixes accompanied by captivating visuals. Explore the sounds of the past and the cutting-edge tunes of today as we curate the ultimate internet radio show experience. Subscribe now and dive into the groove.
-                        </p> */}
-                        <p className="profile__description--text">Bartender by day and DJ by night, you’ll find me spinning beats at some of the best bars and venues across San Diego, CA. Join me on a sonic journey through underground rhythms and timeless house classics.</p>
-                        <br />
-                        <p className="profile__description--text">My YouTube channel is your destination for electrifying Latin house mixes paired with captivating visuals, creating an immersive vibe you can feel.</p>
-                        <br />
-                        <p className="profile__description--text">From iconic tracks of the past to the freshest sounds of today, we’re curating the ultimate internet radio show experience.</p>
-                        <br />
-                        <p className="profile__description--text">🔥 Subscribe now and dive into the groove with Diagnostic & Friends — where the music never stops.</p>
+                    <div className="profile__grid">
+                        <img src={JThirteen} alt="" className="profile__main-image" />
+                        <img src={JSixteen} alt="" className="profile__secondary-image"/>
+                        <div className="profile__description">
+                            <h1 className="profile__description--title">Jose Flores</h1>
+                            <p className="profile__description--text">Bartender by day and DJ by night, you’ll find me spinning beats at some of the best bars and venues across San Diego, CA. Join me on a sonic journey through underground rhythms and timeless house classics.</p>
+
+                            <p className="profile__description--text">My YouTube channel is your destination for electrifying Latin house mixes paired with captivating visuals, creating an immersive vibe you can feel.</p>
+
+                            <p className="profile__description--text">From iconic tracks of the past to the freshest sounds of today, we’re curating the ultimate internet radio show experience.</p>
+
+                            <p className="profile__description--text">🔥 Subscribe now and dive into the groove with Diagnostic & Friends — where the music never stops.</p>
+                        </div>
                     </div>
                 </div>
             </section>
