7c4bf2d637170c9ef93764a522a06a0ad8070c87
Andrew <andrew.mk4@yahoo.com>
2025-12-16 01:52:31 -0600

    update

--- src/pages/Video/Video.jsx
+++ src/pages/Video/Video.jsx
@@ -4,10 +4,6 @@ import CUDiag from "../../assets/CUDiag.jpg";
 import DiagBlur from "../../assets/DiagBlur.jpg";
 import JOne from "../../assets/jOne.jpeg";
 import JThree from "../../assets/jThree.jpg";
-// Using embed iframe for YouTube content instead of vidstack player
-// import { Thumbnail } from "@vidstack/react";
-// import { Time, type TimeProps } from "@vidstack/react";
-// import { Title, type TitleProps } from "@vidstack/react";
 import ScrollToTop from "react-scroll-to-top";
 import FeaturedMix from '../../components/FeaturedMix/FeaturedMix';
 
@@ -55,18 +51,7 @@ function Plans() {
                         </div>
                     </div>
                 </div>
-
                 <div className="video__container" ref={videoRef} id="featured-video">
-                {/* <MediaPlayer playsInline title="Sprite Fight" src="https://files.vidstack.io/sprite-fight/720p.mp4">
-                    <MediaProvider />
-                        <Poster 
-                            className="vds-poster"
-                            src="https://files.vidstack.io/sprite-fight/poster.webp" 
-                            alt="Girl walks into campfire with gnomes surrounding her friend ready for their next meal!" />
-                        <DefaultVideoLayout thumbnails="https://files.vidstack.io/sprite-fight/thumbnails.vtt" icons={defaultLayoutIcons} 
-                        />
-                </MediaPlayer> */}
-                {/* <img src={CUDiag} alt="CUDiag" /> */}
                 <div className="video__container--media">
                     <FeaturedMix poster={CUDiag} embedUrl="https://www.youtube.com/embed/SK6WN-y5P-0" title="Diagnostic - YouTube" start={537} shouldPlay={played} />
                 </div>
