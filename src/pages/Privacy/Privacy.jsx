f1317728fce6e135f35c23935b804c9399cab2f3
Andrew <andrew.mk4@yahoo.com>
2026-02-25 02:34:18 -0600

    update

--- src/pages/Privacy/Privacy.jsx
+++ src/pages/Privacy/Privacy.jsx
@@ -3,25 +3,12 @@ import "./Privacy.css"
 import PersonList from "../../components/PersonList/PersonList";
 import ScrollToTop from "react-scroll-to-top";
 
-// function Privacy() {
-
-//     useEffect(() => {
-//         window.scrollTo(0, 0);
-//     }, []);
-
-//     return (
-//         <div className="legal">
-//             <PersonList />
-//             <ScrollToTop className="hero__scroll" />
-//         </div>
-//     );
-// };
-
 function Privacy() {
 
     useEffect(() => {
         window.scrollTo(0, 0);
     }, []);
+    
     return (
         <div className="legal">
             {/* <PersonAdd /> */}
