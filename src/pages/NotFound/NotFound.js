5ddbddca0e6b3e4bb960ef8312f69f9cea183f0b
Andrew <andrew.mk4@yahoo.com>
2026-03-07 02:21:05 -0600

    update

--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -5,17 +5,25 @@ import "./NotFound.css";
 export default function NotFound() {
     return (
         <div className="notfound-container">
-            <h1 className="notfound-code">404</h1>
+            <h1 className="glitch" data-text="404">
+                404
+            </h1>
 
-            <h2 className="notfound-title">
-                Track Not Found
+            <h2 className="subtitle">
+                SIGNAL LOST
             </h2>
 
-            <p className="nofound-text">
-                Looks like this page got lost in the mix.
+            <p className="description">
+                Looks like this page got lost in the mix. Tis page drifted off the frequency.
             </p>
 
-            <Link to="/" className="notfound-button">
+            <div>
+                {Array.from({ length: 20 }).map((_, i) => {
+                    <span key={i}></span>
+                })}
+            </div>
+
+            <Link to="/" className="home-button">
                 Return to Diagnostic
             </Link>
         </div>
