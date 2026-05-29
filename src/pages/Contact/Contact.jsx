2cf488adfd6d7beaca57c3cfbafb8b3c446eeb44
Andrew <andrew.mk4@yahoo.com>
2026-05-29 17:30:56 -0500

    new background

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,22 +1,22 @@
 import React from "react";
 import "./Contact.css";
-import jFifteenInk from "../../assets/jFifteenInk.jpg";
+import jFifteen from "../../assets/jFifteen.jpg";
 import ContactForm from "../../components/ContactForm/ContactForm";
 
 const Contact = () => {
   return (
     <main className="contact">
-      <section className="contact__hero">
+      {/* <section className="contact__hero">
         <div className="contact__intro">
           <h1 className="contact__title">Contact</h1>
         </div>
-      </section>
+      </section> */}
 
       <section>
         {/* use container background-image so cover is reliable */}
         <div
           className="contact__wall"
-          style={{ backgroundImage: `url(${jFifteenInk})` }}
+          style={{ backgroundImage: `url(${jFifteen})` }}
         >
           <div className="contact__form-overlay" aria-hidden={false}>
             <div className="contact__form-card">
