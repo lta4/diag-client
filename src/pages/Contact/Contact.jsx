4977ea68e7004cc811d1cc2271d760f114827f46
Andrew <andrew.mk4@yahoo.com>
2026-05-29 00:17:55 -0500

    contact form over image

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -4,26 +4,29 @@ import jFifteenInk from "../../assets/jFifteenInk.jpg";
 import ContactForm from "../../components/ContactForm/ContactForm";
 
 const Contact = () => {
-    return (
-        <>
-            <main className="contact">
-                <section className="contact__hero">
-                    <div className="contact__intro">
-                        <h1 className="contact__title">Contact</h1>
-                        <ContactForm />
-                    </div>
-                </section>
-                <section>
-                    <div className="contact__wall">
-                        <img src={jFifteenInk}
-                            alt="J Fifteen Ink"
-                            className="contact__wall--image"
-                        />
-                    </div>
-                </section>
-            </main>
-        </>
-    );
+  return (
+    <main className="contact">
+      <section className="contact__hero">
+        <div className="contact__intro">
+          <h1 className="contact__title">Contact</h1>
+        </div>
+      </section>
+
+      <section>
+        {/* use container background-image so cover is reliable */}
+        <div
+          className="contact__wall"
+          style={{ backgroundImage: `url(${jFifteenInk})` }}
+        >
+          <div className="contact__form-overlay" aria-hidden={false}>
+            <div className="contact__form-card">
+              <ContactForm />
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
 };
 
 export default Contact;
