bec45c66dd71147eb55cd79ab6a5a4367edab0a7
Andrew <andrew.mk4@yahoo.com>
2026-07-04 16:50:55 -0500

    upate gallery

--- src/data/galleryData.js
+++ src/data/galleryData.js
@@ -8,8 +8,10 @@ import jFour from "../assets/jFour.jpg";
 import jNine from "../assets/jNine.jpeg";
 import jTen from "../assets/jTen.jpeg";
 import jSixteen from "../assets/jSixteen.jpg";
+import DNP8 from "../assets/DNP8.jpeg";
 import DNP32 from "../assets/DNP32.jpeg";
 import DNP36 from "../assets/DNP36.jpeg";
+import DNP50 from "../assets/DNP50.jpeg";
 
 const IMAGES = [
   { src: jOne, name: "San Diego @ Maverick's" },
@@ -20,8 +22,10 @@ const IMAGES = [
   { src: jNine, name: "San Diego @ Mavericks's" },
   { src: jTen, name: "San Diego @ Mavericks's" },
   { src: jSixteen, name: "Jose 3" },
-  { src: DNP32, name: "DNP32"},
-  { src: DNP36, name: "DNP36"}
+  { src: DNP8, name: "DNP8" },
+  { src: DNP32, name: "DNP32" },
+  { src: DNP36, name: "DNP36" },
+  { src: DNP50, name: "DNP50" }
 ];
 
 export default IMAGES;
