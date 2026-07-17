8e4e59685bce96a352f4aee1967a6ea60783d79a
Andrew <andrew.mk4@yahoo.com>
2026-07-17 02:31:00 -0500

    update gallery

--- src/data/galleryData.js
+++ src/data/galleryData.js
@@ -7,6 +7,7 @@ import jFour from "../assets/jFour.jpg";
 // import jFive from "../assets/jFive.jpg";
 import jNine from "../assets/jNine.jpeg";
 import jTen from "../assets/jTen.jpeg";
+import jEleven from "../assets/jEleven.jpeg";
 import jSixteen from "../assets/jSixteen.jpg";
 import DNP8 from "../assets/DNP8.jpeg";
 import DNP32 from "../assets/DNP32.jpeg";
@@ -16,16 +17,17 @@ import DNP50 from "../assets/DNP50.jpeg";
 const IMAGES = [
   { src: jOne, name: "San Diego @ Maverick's" },
   { src: jTwo, name: "San Diego @ Sycamore Den" },
-  { src: jThree, name: "San Diego @ Sycamore Den" },
+  { src: DNP32, name: "DNP32" },
   { src: jFour, name: "San Diego @ Mavericks's" },
   // { src: jFive, name: "jFive.jpg" },
   { src: jNine, name: "San Diego @ Mavericks's" },
   { src: jTen, name: "San Diego @ Mavericks's" },
   { src: jSixteen, name: "Jose 3" },
   { src: DNP8, name: "DNP8" },
-  { src: DNP32, name: "DNP32" },
   { src: DNP36, name: "DNP36" },
-  { src: DNP50, name: "DNP50" }
+  { src: jThree, name: "San Diego @ Sycamore Den" },
+  { src: DNP50, name: "DNP50" },
+  { src: jEleven, name: "San Diego @ Sycamore Den" }
 ];
 
 export default IMAGES;
